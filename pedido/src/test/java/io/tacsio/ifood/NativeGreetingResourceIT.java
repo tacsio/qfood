6b9fa8f36bcaaf5fd5721a9967bc85daa3e9e5e3
Tarcisio Coutinho <tcs5cin@gmail.com>
2021-07-27 16:30:04 -0300

    initial commmit

---
+++ pedido/src/test/java/io/tacsio/ifood/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package io.tacsio.ifood;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
